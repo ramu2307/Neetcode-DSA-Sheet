90e5c94bf138e82b08c507492bee692ea016e67e
Raja Ram <2374762@cognizant.com>
2025-06-09 22:42:36 +0530

    Trees

---
+++ Trees/Binary_Tree_Postorder_Traversal.java
@@ -0,0 +1,39 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Binary_Tree_Postorder_Traversal {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+
+        private void postOrder(TreeNode root, List<Integer> ans) {
+            if(root == null) {
+                return;
+            }
+
+            postOrder(root.left, ans);
+            postOrder(root.right, ans);
+            ans.add(root.val);
+        }
+        public List<Integer> postorderTraversal(TreeNode root) {
+            List<Integer> ans = new ArrayList<>();
+            postOrder(root, ans);
+            return ans;
+        }
+    }
+    
+}
